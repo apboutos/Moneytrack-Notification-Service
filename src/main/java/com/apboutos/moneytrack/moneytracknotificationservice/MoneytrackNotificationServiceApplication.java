fef5fbfbaea5efafb5ec226d69c8b1cc5dc38df4
Apostolos Boutos <apboutos@gmail.com>
2024-06-18 19:18:31 +0300

    Initial Commit

---
+++ src/main/java/com/apboutos/moneytrack/moneytracknotificationservice/MoneytrackNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.apboutos.moneytrack.moneytracknotificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneytrackNotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneytrackNotificationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/apboutos/moneytrack/moneytracknotificationservice/MoneytrackNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apboutos.moneytrack.moneytracknotificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneytrackNotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
